4f1da421c2ed3d6e09898940be1bc4e46b65228f
Alexandre TL <alextorresleguet@icloud.com>
2024-09-21 09:11:07 +0200

    mup coord check

---
+++ test_mup.py
@@ -0,0 +1,82 @@
+from contextlib import nullcontext
+
+import random
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+from models.lm import LM
+
+from data.dataloader import DataLoader
+from utils.mup.coord_check import get_coord_data, plot_coord_data
+from config import *
+
+# -------------
+
+lr = 2e-3
+
+batch_size = 64
+
+max_value = 100
+
+widths = [72, 144, 252, 516, 768, 1020]
+# check that for all these widths, d_model is divisible by d_head
+
+# -------------
+
+d_head_0 = config.d_head # fixed through scaling
+config.mup_base_width = widths[0]
+
+seed = 123456789 + seed
+
+random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
+torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
+device_type = "cuda" if "cuda" in device else "cpu"
+torch_dtype = {"float32": torch.float32, "float16": torch.float16, "bfloat16": torch.bfloat16}[dtype]
+dtype_ctx = (nullcontext() if device_type == "cpu" else torch.amp.autocast(device_type, torch_dtype))
+
+class RandomDataset(Dataset):
+    def __len__(self):
+        return 9999999
+
+    def __getitem__(self, idx):
+        data = torch.randint(low=0, high=max_value, size=(batch_size, ctx_len))
+        x = data[:, :-1].int()
+        y = data[:, 1:].long()
+        return x, y
+
+def lazy_model(width):
+    config.d_model = width
+    if architecture == "Transformer":
+        config.d_ff = int((8/3) * width)
+    
+    if architecture == "Transformer" or architecture == "Mamba2":
+        config.n_heads = width//d_head_0
+
+    print(config.d_head)
+    
+    return lambda: LM(config, vocab_size=max_value).to(device) # todo
+
+models = {width: lazy_model(width) for width in widths}
+
+dataset = RandomDataset()
+loader = DataLoader(dataset, batch_size=None, shuffle=True)
+iter_ = iter(loader)
+
+optcls = lambda model: model.configure_optimizers(weight_decay, lr, (adam_b1, adam_b2), device_type)
+
+df = get_coord_data(models, iter_, optcls, nsteps=10)
+
+if use_mup:
+    name = "mup.png"
+else:
+    name = "no_mup.png"
+
+plot_coord_data(df, legend="auto", save_to=name)
--- train.py
+++ train.py
@@ -1,10 +1,8 @@
 """
 
 main training script.
-default config is XXX
 
 doesnt support :
--gradient_acc
 -multi GPU
 
 when launching a training run, will create a name for the run (wandb run or random if no wandb logging) and save checkpoint, config and final model in runs/{run_name}
@@ -29,108 +27,15 @@ import time
 import wandb
 
 import torch
-import torch.nn.functional as F
 import torch.optim.lr_scheduler as lr_scheduler
 import torch._inductor.config as torch_ind_config
 
 from utils.lr_schedules import cosine_warmup_schedule, wsd_schedule
 
 from models.lm import LM
-from models.transformer.transformer import TransformerConfig
-from models.mamba.mamba import MambaConfig
-from models.mamba.mamba2 import Mamba2Config
-
 from data.dataloader import DataLoader
-
 from utils.misc import format_time
-
-# ---------------------------------------------
-
-seed = 0 # 0, 1, 2...
-
-# --- downstream eval parameters ---
-#eval_interval = 1000
-#num_tasks = 100
-
-# --- data ---
-vocab_size = 50257 # gpt2 enc
-ctx_len = 1024
-
-# --- model parameters ---
-architecture = "Transformer" # "Transformer" or "Mamba" or "Mamba2"
-d_model = 768
-n_layers = 12
-base_std = 0.02
-
-# Mamba specific
-use_cuda = True # choose True if you can (mamba-ssm installed). else, fallbacks to mamba.py (https://github.com/alxndrTL/mamba.py)
-
-# Mamba2 specific
-bias = False
-d_head = 64
-d_state = 128
-
-# Transformer specific
-d_ff = 2048
-n_heads = 12
-n_kv_heads = 12 # n_heads is MHA, 1 is MQA (multi query), in between is GQA (grouped query attention)
-dropout = 0.
-
-pos_emb = "rope" # "absolute" or "rope"
-rope_theta = 10000
-
-use_flash_attention = True
-
-# --- muP parameters ---
-use_mup = False
-mup_base_width = 288
-
-# --- training parameters ---
-num_iters = 9536
-total_batch_size = 512
-micro_batch_size = 16
-
-optimizer = "AdamW" # "AdamW" or "Adam-mini"
-
-# LR and scheduler
-schedule = "wsd" # "cosine" or "wsd"
-
-lr = 1.8e-3
-lr_warmup_iters = 256
-
-# cosine schedule specific
-lr_min = 1.8e-4
-
-# wsd schedule specific
-lr_decay_iters = 2048 # 10-20% of num_iters
-
-adam_b1 = 0.9
-adam_b2 = 0.95
-
-max_grad_norm = 1.0
-weight_decay = 0.1
-
-use_torch_compile = True # do not toggle if using Mamba
-
-device = "cuda" # "cpu", "cuda:0", "cuda:1", ...
-dtype = "bfloat16" # "float32" or "bfloat16"
-
-# --- saving/checkpointing parameters ---
-save_dir = "runs/" # where to save to
-ckpt_interval = 1000 # None if you don't want checkpointing
-# size of each checkpointing file, in MB : 12 * number of parameters (in M)
-
-ckpt = "" # if you want to restart training from a checkpoint (path/to/model.pth)
-start_iter = 0 # specify starting iter (if loading from ckpt_60000, put 60001)
-
-# --- logging and eval parameters ---
-log_wandb = True
-
-train_log_interval = 12
-eval_val_interval = 12 # also the printing period
-eval_val_iters = 50
-
-# ---------------------------------------------
+from config import *
 
 seed = 123456789 + seed
 
@@ -215,16 +120,7 @@ print(f"Tokens processed per iteration: {tokens_per_iter}")
 print(f"Number of micro batches: {grad_acc_steps}")
 
 # model
-if architecture == "Transformer":
-    config = TransformerConfig(d_model=d_model, n_layers=n_layers, n_heads=n_heads, n_kv_heads=n_kv_heads, d_ff=d_ff, pos_emb=pos_emb, rope_theta=rope_theta, base_std=base_std, mup=use_mup, mup_base_width=mup_base_width, dropout=dropout, max_len=ctx_len, flash=use_flash_attention)
-elif architecture == "Mamba":
-    config = MambaConfig(d_model=d_model, n_layers=n_layers, bias=bias, base_std=base_std, mup=use_mup, mup_base_width=mup_base_width, use_cuda=use_cuda)
-elif architecture == "Mamba2":
-    config = Mamba2Config(d_model=d_model, n_layers=n_layers, d_state=d_state, d_head=d_head, n_groups=1, max_len=ctx_len, bias=bias, base_std=base_std, mup=use_mup, mup_base_width=mup_base_width)
-else:
-    raise NotImplementedError
-
-config_dict = asdict(config)
+config_dict = asdict(config) # config is defined in config.py
 config_dict['architecture'] = architecture
 json.dump(config_dict, open(os.path.join(save_dir, 'config.json'), 'w'))
 
